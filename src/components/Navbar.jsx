23cc2af4db441a56dc0e87c389bdf4d84b268bcd
Mariana Franco <marianamedeirosfranco@gmail.com>
2023-03-15 10:18:32 +0000

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,25 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/auth.context';
+import '../components/navbar.css'
 
 function Navbar() {
   const { loggedIn, user, logout } = useContext(AuthContext);
 
   return (
-    <nav>
-      <Link to="/"> Home </Link>
+    <nav className="nav-bar">
+      <Link className="nav-items" to="/"> Home </Link>
       {loggedIn ? (
         <>
           <span>Hello {user.name}</span>
-          <Link to="/tasks"> Tasks </Link>
-          <Link to="/tasks/new"> Add a Task </Link>
+          <Link className="nav-items" to="/tasks"> Tasks </Link>
+          <Link className="nav-items" to="/tasks/new"> Add a Task </Link>
           <button onClick={logout}>Logout</button>
         </>
       ) : (
         <>
-          <Link to="/signup"> Signup </Link>
-          <Link to="/login"> Login </Link>
+          <Link className="nav-items" to="/signup"> Signup </Link>
+          <Link className="nav-items" to="/login"> Login </Link>
         </>
       )}
     </nav>
