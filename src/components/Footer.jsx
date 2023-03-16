5c2ee5329abf331c4c356b095d9521e7ff75aad0
Francisco Manuel Pereira <70414789+FranciscoManuelPereira@users.noreply.github.com>
2023-03-16 11:38:05 +0000

    remove win32 package

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import '../components/footer.css'
+import React from "react";
+import "../components/footer.css";
+import LogoImg from "../assets/TocheckLogo.png";
 
 function Footer() {
   return (
-<footer>
-    <div className="footer">
-        <img className="footer-img" src="/src/assets/ToCheckLogo.png" alt="footer logo image"/>
+    <footer>
+      <div className="footer">
+        <img className="footer-img" src={LogoImg} alt="footer logo image" />
         <div className="footer-body">
-            <p className="card-text">TOCHÉ in your life flow!</p>
-            <p className="card-text">contact@tocheck.com</p>
-            <p className="card-text"><small class="text-muted">
-            <span>Developed by <a href="https://github.com/marianamedeirosfranco">Mariana Franco</a> and <a href="https://github.com/franciscomanuelpereira">Francisco Pereira</a></span></small></p>
+          <p className="card-text">TOCHÉ in your life flow!</p>
+          <p className="card-text">contact@tocheck.com</p>
+          <p className="card-text">
+            <small class="text-muted">
+              <span>
+                Developed by{" "}
+                <a href="https://github.com/marianamedeirosfranco">
+                  Mariana Franco
+                </a>{" "}
+                and{" "}
+                <a href="https://github.com/franciscomanuelpereira">
+                  Francisco Pereira
+                </a>
+              </span>
+            </small>
+          </p>
         </div>
-    </div>
-</footer>
-  )
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
